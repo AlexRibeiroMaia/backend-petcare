8587c0d574766aa98a22a61ba6afa332ec0f12e7
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2026-03-07 07:05:29 -0300

    adicionando notacao de repository

--- src/main/java/com/petcare/pet_care/adapters/outbound/repositories/JpaConsultationRepository.java
+++ src/main/java/com/petcare/pet_care/adapters/outbound/repositories/JpaConsultationRepository.java
@@ -4,11 +4,13 @@ import com.petcare.pet_care.adapters.outbound.entities.JpaConsultationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface JpaConsultationRepository extends JpaRepository<JpaConsultationEntity, Long> {
     List<JpaConsultationEntity> findByPetIdOrderByDateDesc(UUID petId);
 
